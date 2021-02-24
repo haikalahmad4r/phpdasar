99a727d60034f28ea5c2d204005c850825c547d8
haikalahmad4r <haikalshinji@gmail.com>
2021-02-24 15:56:25 +0700

    initialization commit

---
+++ dsasar.php
@@ -0,0 +1,11 @@
+<?php
+
+
+define("phi 3.14")
+$angka = 1;
+
+$nama = haikal;
+echo 'ini adalah ' . $angka;
+echo "</br>";
+echo $nama;
+?>
---
+++ variabel_konstan.php
@@ -0,0 +1,12 @@
+<?php
+//definisikan konstanta
+define ('PHI' , 3.14);
+define ('DBNAM' , 'inventori');
+define('DBSERVER','localhost');
+
+$jari =8;
+$luas = PHI * $jari * $jari;
+$kll = 2 * PHI * $jari;
+
+echo 'luas lingkarangan adalah' .$luas;
+?>
